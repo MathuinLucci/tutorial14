76a179ccebf2cac02c2a9e7a8aef01ae5a0aabe7
MathuinLucci <matt.parisano@gmail.com>
2025-05-02 09:08:09 -0500

    fixed typos, and for some reason there were images linked in css

--- js/ag_cards.js
+++ js/ag_cards.js
@@ -60,7 +60,7 @@ function pokerCard(cardSuit, cardRank) {
 /* Method to reference the image source file for a card */
 pokerCard.prototype.cardImage = function() {
    var suitAbbr = this.suit.substring(0, 1).toLowerCase();
-   return suitAbbr + this.rankValue + ".png";
+   return "./img/" + suitAbbr + this.rankValue + ".png";
 };
 
 /* method to replace a card with one from the deck */
